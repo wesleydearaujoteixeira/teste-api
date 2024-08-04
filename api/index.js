d4c0065f1090331279377a0ede0315b9496bbbe2
Wesley <wesleydearaujoteixeira@gmail.com>
2024-08-03 21:26:08 -0300

    sdsds sdsrfdfgdgdsag

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const PORT = 3000; // 
+const PORT = 5000; // 
 const router = require('../routes/UserRoutes');
 const PetRouter = require('../routes/PetRouters');
 
